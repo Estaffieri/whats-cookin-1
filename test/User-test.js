4206749197183fbbe5ecb0ea0eba537fda9383b2
Estelle Staffieri <estellelareinne@gmail.com>
2020-10-12 18:20:29 -0600

    Rename user dummy data file and make describe block for user test

--- test/User-test.js
+++ test/User-test.js
@@ -1,5 +1,17 @@
 const chai = require("chai");
 const expect = chai.expect;
 
-const require("../src/User");
-const require("../src/User");
+const User = require("../src/User");
+const testUsers = require("../dummy-data/testUsers");
+
+describe("User", function() {
+    let id, name, pantry, favoriteRecipes;
+    beforeEach(function() {
+        user = new Card(dummyData.id, dummyData.name, dummyData.pantry, favoriteRecipes)
+    });
+
+  it('should be a function', function() {
+    expect(User).to.be.a('function');
+  });
+  
+});
