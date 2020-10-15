26405c8a6daa62b15791579a8ced37d1c8b2036a
Bruce Gordon <brc.gordon@gmail.com>
2020-10-15 14:55:13 -0600

    Add dom variables through querySelectors

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,17 @@
-console.log('Hello world');
+const homeButton = document.querySelector("#home");
+const favoritesButton = document.querySelector("#favorites");
+const welcome = document.querySelector(".welcome-user");
+const searchBar = document.querySelector(".search");
+
+const sectionHeading = document.querySelector(".recipes-heading");
+
+const homeView = document.querySelector(".home-view");
+const favoriteView = document.querySelector(".favorite-view");
+const singleCategoryView = document.querySelector(".single-category-view");
+const recipeView = document.querySelector(".recipe-view");
+
+const checkPantryButton = document.querySelector("#pantry-button");
+const getShoppingList = document.querySelector("#shopping-list-button");
+const recipeImage = document.querySelector(".recipe-image");
+const ingredientList = document.querySelector(".ingredient-list");
+const directionsList = document.querySelector(".directions-list")
